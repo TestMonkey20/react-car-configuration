cc0675bc198e8d7552b6924f582d1c15413004de
TestMonkey20 <aleksander.grodecki@gmail.com>
2021-07-31 00:54:34 +0200

    Summary display

--- src/Summary.js
+++ src/Summary.js
@@ -1,10 +1,44 @@
 import './App.scss'
+import { engines, caroseryLacquer, rimLacquer, rimSize, seats, upholsteryColor  } from './SetupOptions'
+import { useSelector } from 'react-redux'
 
 function Summary() {
 
+	const config = useSelector(store => store.configuration)
+
+	const engineCost = engines.find(e => e.name == config.engine).cost
+	const carroseryLacquerCost = caroseryLacquer.find(e => e.name == config.caroseryLacquer).cost
+	const rimLacquerCost = rimLacquer.find(e => e.name == config.rimLacquer).cost
+	const rimSizeCost = rimSize.find(e => e.name == config.rimSize).cost
+	const seatsCost = seats.find(e => e.name == config.seats).cost
+	const upholsteryColorCost = upholsteryColor.find(e => e.name == config.upholsteryColor).cost
+
 	return (<div>
 		<div>
-			
+			<ul>
+				<li>Engine: {config.engine}, {engineCost} zł</li>
+				<li>Caroserylacquer: {config.caroseryLacquer}, {carroseryLacquerCost} zł</li>
+				<li>Rim lacquer: {config.rimLacquer}, {rimLacquerCost} zł</li>
+				<li>Rim size: {config.rimSize}, {rimSizeCost} zł</li>
+				<li>Seats: {config.seats}, {seatsCost} zł</li>
+				<li>Upholstery color: {config.upholsteryColor}, {upholsteryColorCost} zł</li>
+				{config.dynamicLED ? <li>Dynamic LED: 13000 zł</li> : null}
+				{config.premiumSound ? <li>Premium Sound: 14000 zł</li> : null}
+				{config.sportSuspension ? <li>Sport suspension: 15000 zł</li> : null}
+			</ul>
+			<div>
+				Total cost: {	
+					engineCost 
+					+ carroseryLacquerCost 
+					+ rimLacquerCost 
+					+ rimSizeCost 
+					+ seatsCost 
+					+ upholsteryColorCost
+					+ (config.dynamicLED ? 13000 : 0)
+					+ (config.premiumSound ? 14000 : 0)
+					+ (config.sportSuspension ? 15000 : 0)
+				} zł
+			</div>
 		</div>
 	</div>)
 
